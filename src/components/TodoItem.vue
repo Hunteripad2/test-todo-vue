b043bed6df9510bfcf42c9b6dae3d580ebe7734f
Juju Man <hunteripad2@gmail.com>
2021-03-12 00:59:48 +0300

    Initial

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="form-check todo_item mt-5 me-auto ms-auto col-auto">
+        <input type="checkbox" class="form-check-input" />
+        <label class="form-check-label">{{newTodoText}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "todo-item",
+    props: {
+        newTodoText: String,
+    },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="row" id="todo_list"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "todo-list",
+    //props: {
+    //    newTodoText: String,
+    //},
+};
+</script>
